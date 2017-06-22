09e436dd3c65264febb0f74fd11c0d454ff48fb3
Nicholas Rogé <NicholasRoge@gmail.com>
2017-06-21 23:35:08 -0500

    Initialized basic environment.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>^_^</title>
+        
+        <link rel="stylesheet" href="css/app.css" type="text/css" />
+
+        <script src="js/app.js"></script>
+    </head>
+    <body>
+        <canvas id="main"></canvas>
+    </body>
+</html>
